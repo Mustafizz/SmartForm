ab07750a1a91dca25e10210b7c784f853b0f80df
Mustafizz <1431204@iub.edu.bd>
2017-02-14 19:41:21 -0800

    Update smartForm.js

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,38 @@
 // Your jQuery goes here
 
+
+
 var userData = {};
+$("#start").click(function(event){
+  $("#q1").show();
+  $("#welcome").hide();
+});
+$("#nxt").click(function(event){
+  $("#q2").show();
+  $("#q1").hide();
+});
+$("#ht").click(function(event){
+  $("#q2a").show();
+  $("#q2").hide();
+});
+$("#htprv").click(function(event){
+  $("#q2").show();
+  $("#q2a").hide();
+});
+$("#htnxt").click(function(event){
+  $("#thanks").show();
+  $("#q2a").hide();
+});
+$("#cs").click(function(event){
+  $("#q2b").show();
+  $("#q2").hide();
+});
+$("#csprv").click(function(event){
+  $("#q2").show();
+  $("#q2b").hide();
+});
+$("#csnxt").click(function(event){
+  $("#thanks").show();
+  $("#q2b").hide();
+});
+
